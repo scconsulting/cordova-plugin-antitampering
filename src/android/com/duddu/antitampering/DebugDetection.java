76c349ccaf7442f096ae1c967520127e60454007
rokanraja1 <124552992+rokanraja1@users.noreply.github.com>
2024-03-30 23:35:38 +0530

    frida check on android

--- src/android/com/duddu/antitampering/DebugDetection.java
+++ src/android/com/duddu/antitampering/DebugDetection.java
@@ -1,14 +1,14 @@
 package com.duddu.antitampering;
 
+import android.app.ActivityManager;
 import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.os.Debug;
 
 import java.lang.reflect.Field;
-
+import java.util.List;
 
 class DebugDetection {
-
     public static void check(String packageName, Context context) throws Exception {
         if (isDebuggerPresent(packageName, context)) {
             throw new Exception("Debugger is present");
@@ -18,6 +18,9 @@ class DebugDetection {
         }
         if (getDebugField(packageName)) {
             throw new Exception("App running in Debug mode");
+        }
+        if (isFridaPresent(context)) {
+            throw new Exception("Frida is present");
         }
     }
 
@@ -34,8 +37,7 @@ class DebugDetection {
     private static boolean isDebuggerPresent(String packageName, Context context) {
         try {
             if ((context.getPackageManager().getPackageInfo(
-                    context.getPackageName(), 0).applicationInfo.flags &
-                    ApplicationInfo.FLAG_DEBUGGABLE) != 0) {
+                    context.getPackageName(), 0).applicationInfo.flags & ApplicationInfo.FLAG_DEBUGGABLE) != 0) {
                 //Debug and development mode
                 return true;
             }
@@ -44,5 +46,16 @@ class DebugDetection {
             e.printStackTrace();
             return true;
         }
+    }
+
+    private static boolean isFridaPresent(Context context) {
+        ActivityManager activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningAppProcessInfo> runningProcesses = activityManager.getRunningAppProcesses();
+        for (ActivityManager.RunningAppProcessInfo processInfo : runningProcesses) {
+            if (processInfo.processName.equals("frida-server")) {
+                return true;
+            }
+        }
+        return false;
     }
 }
